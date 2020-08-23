0d65f7742ecbd40d55044a2de98282e92a26d6c2
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-23 12:41:02 -0700

    included index+employeeRoutes -> routes folder

---
+++ routes/employeeRoutes.js
@@ -0,0 +1,25 @@
+// 1 -> bring in express for router purposes
+const router = require('express').Router()
+
+// GET all items
+router.get('/employees', (req, res) => {
+
+})
+
+
+// POST one item
+router.post('/employees', (req, res) => {
+
+})
+
+
+// PUT one item
+router.put('/employees/:id', (req, res) => {
+
+})
+
+
+// DELETE one item
+router.delete('/employees/:id', (req, res) => {
+
+})
---
+++ routes/index.js
@@ -0,0 +1,3 @@
+// 1st -> bring in express for router purposes
+const router = require('express').Router()
+
